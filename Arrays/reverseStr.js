59c7c215d2aa6b4a2cae17d91d8d9d1ac48e3f3e
Edet Asuquo <eddy1759@gmail.com>
2023-01-17 00:00:42 +0100

    commit

--- Arrays/reverseStr.js
+++ Arrays/reverseStr.js
@@ -1,19 +1,19 @@
-// create a function that reverses a string:
-// 'Hi My name is Edet' should be:
-// 'tedE si eman yM iH'
-
-function reverse(str) {
+function reverseString(str){
     if(!str || str.length < 2 || typeof str != 'string'){
-        return "Wrong format"
+        return "Wrong input type"
     }
 
     const backwards = []
     const totalItems = str.length - 1
+
     for (let i = totalItems; i >= 0; i--){
         backwards.push(str[i])
     }
-    return backwards
 
+    return backwards.join('')
 }
 
-reverse('Hi My name is Edet')
+function reverseString2(str){
+    return str.split('').reverse().join('')
+}
+
